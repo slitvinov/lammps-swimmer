6f9fa96a52cec3554409afb01a0acc46cbb0230c
Litvinov Sergey <slitvinov@gmail.com>
2014-08-26 23:53:33 +0200

    Clearn up

--- src/USER-SPH/fix_extfield.h
+++ src/USER-SPH/fix_extfield.h
@@ -28,44 +28,20 @@ class FixExtField : public Fix {
  public:
   FixExtField(class LAMMPS *, int, char **);
   ~FixExtField();
-  int setmask();
-  void init();
-  void setup(int);
   void post_force(int);
   void post_force_respa(int, int, int);
-  void post_force_setup(int);
-  void post_force_respa_setup(int, int, int);
-  void end_of_step();
-  void reset_dt();
-  void write_restart(FILE *);
-  void restart(char *);
-  int pack_restart(int, double *);
-  void unpack_restart(int, int);
-  int size_restart(int);
-  int maxsize_restart();
+  int setmask();
+  void init();
   double memory_usage();
-  void grow_arrays(int);
   double compute_vector(int);
 
  private:
   int me;
-  int nfileevery;
   int nlevels_respa;
-  int seed;
-  class RanMars *random;
-  FILE *fp,*fpr;
+  FILE *fpr;
   int nxnodes,nynodes,nznodes,total_nnodes;
-  int ***nsum;
-  int ***nsum_all,***T_initial_set;
-  double *gfactor1,*gfactor2,*ratio;
-  double **flangevin;
-  double ***T_electron,***T_electron_old;
-  double ***sum_vsq,***sum_mass_vsq;
-  double ***sum_vsq_all,***sum_mass_vsq_all;
-  double ***net_energy_transfer,***net_energy_transfer_all;
-  double electronic_specific_heat,electronic_density;
-  double electronic_thermal_conductivity;
-  double gamma_p,gamma_s,v_0,v_0_sq;
+  int ***T_initial_set;
+  double ***T_electron;
 
   void read_initial_electron_temperatures();
 };
