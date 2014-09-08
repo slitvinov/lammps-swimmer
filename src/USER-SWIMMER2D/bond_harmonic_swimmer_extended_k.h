58800956a5215ac2285bd2ffa0ee07eccc1c90e4
Tiago Faria <tiago_gfaria@hotmail.com>
2014-09-08 21:58:29 +0200

    add bond_harmonic_swimmer_extended_k.h

---
+++ src/USER-SWIMMER2D/bond_harmonic_swimmer_extended_k.h
@@ -0,0 +1,53 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef BOND_CLASS
+
+BondStyle(harmonic/swimmer/extended/k,BondHarmonicSwimmerExtendedK)
+
+#else
+
+#ifndef LMP_BOND_HARMONIC_SWIMMER_EXTENDED_K_H
+#define LMP_BOND_HARMONIC_SWIMMER_EXTENDED_K_H
+
+#include "stdio.h"
+#include "bond.h"
+
+namespace LAMMPS_NS {
+
+class BondHarmonicSwimmerExtendedK : public Bond {
+ public:
+  BondHarmonicSwimmerExtendedK(class LAMMPS *);
+  virtual ~BondHarmonicSwimmerExtendedK();
+  virtual void compute(int, int);
+  void coeff(int, char **);
+  double equilibrium_distance(int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_data(FILE *);
+  double single(int, double, int, int, double &);
+
+ protected:
+  double *k, *k_alpha, *k_beta, *r0,*r1;
+  double *A_alpha, *A_beta, *omega_alpha, *omega_beta, *phi, *vel_sw;
+  tagint *n1, *n2;
+
+  bigint time_origin;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif
