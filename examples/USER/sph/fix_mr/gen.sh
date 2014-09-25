e6d94cf7fb41508a44c42e4ee8b3401e82caa7be
Litvinov Sergey <slitvinov@gmail.com>
2014-09-25 23:47:08 +0200

    Update of configuration files

--- examples/USER/sph/fix_mr/gen.sh
+++ examples/USER/sph/fix_mr/gen.sh
@@ -9,7 +9,6 @@ convert -flip ${imgfile} tif_flip.txt
 #maxima --very-quiet -r "Ntotal: ${Ntotal}$ batchload(\"gen-multi-resolution.mac\")\$"
 
 awk -v Ntotal=${Ntotal} -f gen.awk tif_flip.txt > table.aux
-paste -d ' 'table.aux lvl.dat | awk '{print $1, $2, $3, $4, $8}' > table.in
+paste -d ' ' table.aux lvl.dat | awk '{print $1, $2, $3, $4, $8}' > table.in
 
-
-#awk -v Ntotal=${Ntotal} -f gen-data.awk tif.txt p.dat > data/sph.data
+awk -v Ntotal=${Ntotal} -f gen-data.awk tif.txt p.dat > data/sph.data
