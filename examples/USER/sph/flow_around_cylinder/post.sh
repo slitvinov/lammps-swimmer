e3da1d758d70fb3fc0ca54883f38fe7616406ac7
Tiago Faria <tiago_gfaria@hotmail.com>
2014-11-22 15:49:42 +0100

    modify config for user

--- examples/USER/sph/flow_around_cylinder/post.sh
+++ examples/USER/sph/flow_around_cylinder/post.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # x, y, fx, fy, colorgradient_peratom
-/home/litvinov/work/lammps-sph/examples/USER/sph/scripts/lammps2punto.sh data-wall/dump*.*  | awk 'NF{print $3, $4, $6, $7, $9, $10, $2, $NF} !NF' > hm
+/home/Thesis/lammps-swimmer-transport-wip/examples/USER/sph/scripts/lammps2punto.sh data-wall/dump*.*  | awk 'NF{print $3, $4, $6, $7, $9, $10, $2, $NF} !NF' > hm
--- examples/USER/sph/flow_around_cylinder/run.sh
+++ examples/USER/sph/flow_around_cylinder/run.sh
@@ -2,5 +2,5 @@
 
 dname=data-wall
 mkdir -p ${dname}
-~/prefix-mpich/bin/mpirun -np 2  ../../../../src/lmp_linux -in flow.lmp -var dname ${dname}
+~/prefix-mpich/bin/mpirun -np 6  ../../../../src/lmp_linux -in flow.lmp -var dname ${dname}
 
